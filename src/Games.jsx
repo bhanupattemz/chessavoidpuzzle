09f53d45fd5657845a44c15dd901b83e84881be9
Bhanu <bhanupattemz@gmail.com>
2025-05-24 15:03:22 +0530

    add puzzle.jsx file

---
+++ src/Games.jsx
@@ -0,0 +1,78 @@
+const games = [
+    {
+        size: [3, 4],
+        pieces: ["bishops", "bishops", "bishops", "bishops", "queen"],
+        notHave: [[0, 0]],
+        solution: [
+            { piece: "bishops", n: [0, 2] },
+            { piece: "bishops", n: [0, 3] },
+            { piece: "bishops", n: [2, 2] },
+            { piece: "bishops", n: [2, 3] },
+            { piece: "queen", n: [1, 0] }
+        ]
+    },
+    {
+        size: [3, 4],
+        pieces: ["king", "king", "knights", "knights", "rooks"],
+        notHave: [[0, 0]],
+        solution: [
+            { piece: "king", n: [0, 3] },
+            { piece: "king", n: [2, 3] },
+            { piece: "knights", n: [0, 1] },
+            { piece: "knights", n: [2, 1] },
+            { piece: "rooks", n: [1, 0] }
+        ]
+    }
+]
+
+const pieces = {
+    king: {
+        moves: [
+            [0, 1], [0, -1], [1, 0], [-1, 0],
+            [-1, -1], [-1, 1], [1, 1], [1, -1]
+        ],
+        num: 1
+    },
+    queen: {
+        moves: [
+            [0, 1], [0, -1], [1, 0], [-1, 0],
+            [-1, -1], [-1, 1], [1, 1], [1, -1]
+        ],
+        num: 64
+    },
+    rooks: {
+        moves: [
+            [0, 1], [0, -1], [1, 0], [-1, 0]
+        ],
+        num: 64
+    },
+    bishops: {
+        moves: [
+            [-1, -1], [-1, 1], [1, 1], [1, -1]
+        ],
+        num: 64
+    },
+    knights: {
+        moves: [
+            [2, 1], [1, 2], [-1, 2], [-2, 1],
+            [-2, -1], [-1, -2], [1, -2], [2, -1]
+        ],
+        num: 1
+    }
+}
+
+const getGame = (level) => {
+    if (level <= 0) {
+        level = 1
+    }
+    if (level > games.length) {
+        level = games.length
+    }
+    return games[level - 1]
+}
+
+const getnLevels = () => {
+    return games.length
+}
+
+export { getGame, pieces, getnLevels }
